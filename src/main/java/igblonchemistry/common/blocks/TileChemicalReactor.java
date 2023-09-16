5ea22a578c3f590fb71999cf6c440536fb754d20
igobylotsofnames <igblon@outlook.com>
2023-09-16 03:33:45 -0700

    Update TileChemicalReactor.java

--- src/main/java/igblonchemistry/common/blocks/TileChemicalReactor.java
+++ src/main/java/igblonchemistry/common/blocks/TileChemicalReactor.java
@@ -4,14 +4,20 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.items.wrapper.CombinedInvWrapper;
 
-public class TileChemicalReactor extends TileEntity {
+public class TileChemicalReactor extends TileEntity implements ITickable {
 
     public static final int SIZE = 2;
+
+    @Override
+    public void update() {
+
+    }
 
     private ItemStackHandler inputHandler = new ItemStackHandler(SIZE) {
         @Override
